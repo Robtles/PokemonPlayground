2f505528376edd2cfa6f4a5753ff0b3eae2f951e
StratRob <robbeyroad@gmail.com>
2017-07-22 23:01:22 +0200

    Reorganizing project

---
+++ PokemonPlayground/PPAppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  PPAppDelegate.swift
+//  PokemonPlayground
+//
+//  Created by Robin on 22/07/2017.
+//  Copyright © 2017 rob. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class PPAppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
