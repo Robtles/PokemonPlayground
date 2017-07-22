f9a2f75c54fa7fd88ce426476db76ac0bd1c7826
Rob <robbeyroad@gmail.com>
2017-07-22 22:33:27 +0200

    Initial Commit

---
+++ PokemonPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonPlayground
+//
+//  Created by Robin on 22/07/2017.
+//  Copyright © 2017 rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
