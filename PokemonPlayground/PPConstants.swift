4225c6300bc2b79c137a64d3381b5946fc366f76
StratRob <robbeyroad@gmail.com>
2017-07-23 21:24:11 +0200

    Main menu view

--- PokemonPlayground/PPConstants.swift
+++ PokemonPlayground/PPConstants.swift
@@ -12,6 +12,15 @@ struct Constants {
     /// Main View title - Navbar
     static let kMainViewNavBarTitle = "Pokémon Playground"
     
+    /// Playground String
+    static let kPlaygroundString = "Playground"
+    
+    /// My Team String
+    static let kMyTeam = "Mon équipe"
+    
+    /// Fetch Pokémons String
+    static let kPokedex = "Pokédex"
+    
     /// ShowMainViewController Segue
     static let kShowMainViewControllerSegue = "showMainViewController"
     
@@ -20,4 +29,23 @@ struct Constants {
     
     /// Application main blue color
     static let kPPApplicationBlueColor = UIColor(hexString: "#C7FFFE")
+    
+    /// Application yellow button
+    static let kPPApplicationYellowButtonColor = UIColor(hexString: "#FFC22C")
+    
+    /// Application blue button
+    static let kPPApplicationBlueButtonColor = UIColor(hexString: "#0061BA")
+    
+    /// Application button font
+    static let kPPApplicationButtonFont = UIFont.boldSystemFont(ofSize: 18.0)
+}
+
+extension Constants {
+    static func ketchumFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont(name: "Ketchum", size: fontSize)!
+    }
+    
+    static func italicKetchumFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont(name: "Ketchum-Italic", size: fontSize)!
+    }
 }
--- PokemonPlayground/PPMainViewController.swift
+++ PokemonPlayground/PPMainViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 open class PPMainViewController: UIViewController {
 
+    @IBOutlet weak var pokeBallImageView: UIImageView?
+    @IBOutlet weak var playgroundLabel: UILabel?
+    @IBOutlet weak var myTeamButton: UIButton?
+    @IBOutlet weak var pokedexButton: UIButton?
+    
     // MARK: - Application lifecycle
     open override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = Constants.kPPApplicationBlueColor
         self.title = Constants.kMainViewNavBarTitle
+        
+        self.pokeBallImageView?.image? = (self.pokeBallImageView?.image?.withRenderingMode(.alwaysTemplate))!
+        self.pokeBallImageView?.tintColor = Constants.kPPApplicationRedColor
+        
+        self.playgroundLabel?.text = Constants.kPlaygroundString
+        self.playgroundLabel?.font = Constants.ketchumFont(ofSize: 40.0)
+        self.playgroundLabel?.textColor = UIColor.black
+        
+        self.myTeamButton?.setTitle(Constants.kMyTeam, for: .normal)
+        self.myTeamButton?.titleLabel?.font = Constants.kPPApplicationButtonFont
+        self.myTeamButton?.backgroundColor = Constants.kPPApplicationYellowButtonColor
+        
+        self.pokedexButton?.setTitle(Constants.kPokedex, for: .normal)
+        self.pokedexButton?.titleLabel?.font = Constants.kPPApplicationButtonFont
+        self.pokedexButton?.backgroundColor = Constants.kPPApplicationBlueButtonColor
     }
 
 }
