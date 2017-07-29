7c70754c43bc7f16a79115d07a7d431169e98cfa
StratRob <robbeyroad@gmail.com>
2017-07-30 00:24:03 +0200

    Handled better Pokemon team view layout for both portrait and landscape orientations

--- PokemonPlayground/PPMyTeamViewController.swift
+++ PokemonPlayground/PPMyTeamViewController.swift
@@ -11,73 +11,132 @@ import UIKit
 private enum ScreenOrientation {
     case portrait
     case landscape
+    
+    var x: CGFloat {
+        switch self {
+        case .portrait: return 2
+        case .landscape: return 3
+        }
+    }
+    
+    var y: CGFloat {
+        switch self {
+        case .portrait: return 3
+        case .landscape: return 2
+        }
+    }
 }
 
+/*
+ * TODO: This class can be highly improved.
+ * I need to have two different layouts for portrait and landscape modes.
+ * Portrait mode: 3 rows of 2 Pokemons, landscape mode: 2 rows of 3 Pokemons.
+ * I tried to do so with AutoLayout, or even using tools like SnapKit, but couldn't get it working...
+ * So I ended up setting the frames directly (but dynamically though). 
+ * Any idea to improve this code would be greatly appreciated.
+ */
 open class PPMyTeamViewController: UIViewController {
     
-    @IBOutlet var containingViews: [UIView]?
-    @IBOutlet var pokemonImageViews: [UIImageView]?
+    var containingViews: [UIView] = []
+    var pokemonImageViews: [UIImageView] = []
     
     // MARK: - Application lifecycle
     open override func viewDidLoad() {
         super.viewDidLoad()
         self.title = Constants.kManageMyTeam
-        self.setupImages()
+    }
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        switch UIDevice.current.orientation {
+        case .landscapeLeft, .landscapeRight:
+            let size = CGSize(width: self.view.size.width,
+                              height: self.view.size.height - self.navigationBarHeight)
+            self.setupView(.landscape, withSize: size)
+            break
+        default:
+            let size = CGSize(width: self.view.size.width,
+                              height: self.view.size.height - UIApplication.shared.statusBarFrame.height - self.navigationBarHeight)
+            self.setupView(.portrait, withSize: size)
+            break
+        }
     }
     
     open override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         
+        // Resetting the view
+        self.containingViews = []
+        self.pokemonImageViews = []
+        self.view.removeSubviews()
+        
         switch UIDevice.current.orientation {
         case .landscapeLeft, .landscapeRight:
-            self.setupView(.landscape)
+            let sizeToUse = CGSize(width: size.width,
+                                   height: size.height + UIApplication.shared.statusBarFrame.height)
+            self.setupView(.landscape, withSize: sizeToUse)
             break
         default:
-            self.setupView(.portrait)
+            let sizeToUse = CGSize(width: size.width,
+                                   height: size.height - self.navigationBarHeight)
+            self.setupView(.portrait, withSize: sizeToUse)
             break
         }
     }
     
     // MARK: - Setup
-    private func setupView(_ orientation: ScreenOrientation) {
-
-        if orientation == .portrait {
-            return
-        } else {
-            return
+    private final func setupView(_ orientation: ScreenOrientation,
+                           withSize size: CGSize) {
+        for i in 0..<6 {
+            self.setupConstraints(forView: i,
+                                  orientation: orientation,
+                                  andSize: size)
         }
     }
     
-    private final func setupImages() {
-        var i = 0
-        for imageView in self.pokemonImageViews! {
-            if var pokemon = self.getTeamPokemonAt(index: i) {
-                if pokemon.hasImageData {
-                    self.pokemonImageViews?[i].image = UIImage(data: (pokemon.imageData)!)
-                } else {
-                    self.pokemonImageViews?[i].kf.indicatorType = .activity
-                    self.pokemonImageViews?[i].kf.setImage(with: URL(string: (pokemon.imageURL)!),
-                                                       completionHandler: { (image, _, _, _) in
-                                                        if image != nil {
-                                                            pokemon.imageData = UIImagePNGRepresentation(image!)
-                                                        }
-                    })
-                }
-                
-                self.pokemonImageViews?[i].setCornerRadius(radius: (self.pokemonImageViews?[i].frame.size.width)! / 2)
-                
-                if let type2 = pokemon.type2 {
-                    self.pokemonImageViews?[i].backgroundColor = type2.color.withAlphaComponent(0.25)
-                } else {
-                    self.pokemonImageViews?[i].backgroundColor = pokemon.type1?.color.withAlphaComponent(0.25)
-                }
+    private final func setupConstraints(forView index: Int, orientation: ScreenOrientation, andSize size: CGSize) {
+        let containingView = UIView(x: size.width / orientation.x * CGFloat(index % Int(orientation.x)),
+                                    y: size.height / orientation.y * CGFloat(index % Int(orientation.y)),
+                                    w: size.width / orientation.x,
+                                    h: size.height / orientation.y)
+        
+        let imageWidth = containingView.frame.size.height * 0.6
+        
+        let imageView = UIImageView(x: (containingView.frame.size.width - imageWidth) / 2,
+                                    y: (containingView.frame.size.height - imageWidth) / 2,
+                                    w: imageWidth,
+                                    h: imageWidth)
+        
+        self.pokemonImageViews.append(imageView)
+        self.containingViews.append(containingView)
+        
+        self.setImageForView(index)
+        
+        containingView.addSubview(imageView)
+        self.view.addSubview(containingView)
+    }
+    
+    private final func setImageForView(_ i: Int) {
+        if let pokemon = self.getTeamPokemonAt(index: i) {
+            if pokemon.hasImageData {
+                self.pokemonImageViews[i].image = UIImage(data: (pokemon.imageData)!)
             } else {
-                imageView.image = UIImage(named: "pokeball")
-                imageView.image? = (imageView.image?.withRenderingMode(.alwaysTemplate))!
-                imageView.tintColor = UIColor.lightGray
+                self.pokemonImageViews[i].kf.indicatorType = .activity
+                self.pokemonImageViews[i].kf.setImage(with: URL(string: (pokemon.imageURL)!))
             }
-
-            i += 1
+            
+            self.pokemonImageViews[i].setCornerRadius(radius: self.pokemonImageViews[i].frame.size.width / 2)
+            
+            if let type2 = pokemon.type2 {
+                self.pokemonImageViews[i].backgroundColor = type2.color.withAlphaComponent(0.25)
+            } else {
+                self.pokemonImageViews[i].backgroundColor = pokemon.type1?.color.withAlphaComponent(0.25)
+            }
+        } else {
+            self.pokemonImageViews[i].image = UIImage(named: "pokeball")
+            self.pokemonImageViews[i].image? = (self.pokemonImageViews[i].image?.withRenderingMode(.alwaysTemplate))!
+            self.pokemonImageViews[i].tintColor = UIColor.lightGray
         }
     }
     
